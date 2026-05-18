cc8f019f5f69c22d04fbe4c3d6fe4687341ee708
Marcus Carvalho <marcussantana03@gmail.com>
2026-05-17 21:25:53 -0300

    feat: protege rotas do carrinho com autenticacao JWT (US-26)

--- src/routes/carrinhoRoute.js
+++ src/routes/carrinhoRoute.js
@@ -7,14 +7,16 @@ const {
   removerItem
 } = require('../controllers/carrinhoController');
 
+const autenticar = require('../middlewares/auth');
+
 const router = express.Router();
 
-router.post('/carrinho/itens', adicionarItem);
+router.post('/carrinho/itens', autenticar, adicionarItem);
 
-router.get('/carrinho', visualizarCarrinho);
+router.get('/carrinho', autenticar, visualizarCarrinho);
 
-router.patch('/carrinho/itens/:itemId', atualizarItem);
+router.patch('/carrinho/itens/:itemId', autenticar, atualizarItem);
 
-router.delete('/carrinho/itens/:itemId', removerItem);
+router.delete('/carrinho/itens/:itemId', autenticar, removerItem);
 
 module.exports = router;
