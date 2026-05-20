de1f88e456c6bdeb73febb6791b3b63ea2ac82d1
Cauã Caldeira <caua.costa.pereira.5@gmail.com>
2026-05-20 18:32:31 -0300

    feat: utlilizar e aplicar cupons

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-const express = require('express');
-
 const pedidosRoutes = require('./src/routes/pedidoRoute');
 const freteRoutes = require('./src/routes/freteRoute');
 const produtosRoutes = require('./src/routes/produtoRoute');
 const carrinhoRoutes = require('./src/routes/carrinhoRoute');
 const usuariosRoutes = require('./src/routes/usuarioRoute');
-
+const cupomRoutes = require('./src/routes/cupomRoute');
+ 
 const cors = require('cors');
-
+ 
 const app = express();
 const PORT = 3000;
-
-
+ 
 app.use(express.json());
 app.use(cors());
-
+ 
 app.use('/api/pedidos', pedidosRoutes);
 app.use('/api/frete', freteRoutes);
 app.use('/api/v1', produtosRoutes);
-
 app.use('/api/v1', carrinhoRoutes);
-
 app.use('/api/v1', usuariosRoutes);
-
+app.use('/api', cupomRoutes);
+ 
 app.get('/', (req, res) => {
   res.json({ mensagem: 'API Bulbe Energia funcionando!' });
 });
-
+ 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
